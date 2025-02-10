085b11ffd620d6c8ec538e3f567f63fa7033def6
taisuke101 <uwattotaitai@gmail.com>
2025-02-10 23:09:56 +0900

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next'
+import path from 'path'
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, '../../../'),
+}
+
+export default nextConfig
